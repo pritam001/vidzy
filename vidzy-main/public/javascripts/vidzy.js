b04a51ac5530f3425d91d811d926463fdc2a825a
Pritam Sarkar <001pritam2012@gmail.com>
2016-05-10 01:31:09 +0530

    Building the Delete page

--- vidzy-main/public/javascripts/vidzy.js
+++ vidzy-main/public/javascripts/vidzy.js
@@ -14,6 +14,10 @@ app.config(['$routeProvider', function($routeProvider){
 	        templateUrl: 'partials/video-form.html',
 	        controller: 'EditVideoCtrl'
 	    })
+	    .when('/video/delete/:id', {
+	        templateUrl: 'partials/video-delete.html',
+	        controller: 'DeleteVideoCtrl'
+	    })
         .otherwise({
             redirectTo: '/'
         });
@@ -54,3 +58,17 @@ app.controller('EditVideoCtrl', ['$scope', '$resource', '$location', '$routePara
         }
     }]);
 
+app.controller('DeleteVideoCtrl', ['$scope', '$resource', '$location', '$routeParams',
+    function($scope, $resource, $location, $routeParams){
+        var Videos = $resource('/api/videos/:id');
+
+        Videos.get({ id: $routeParams.id }, function(video){
+            $scope.video = video;
+        })
+
+        $scope.delete = function(){
+            Videos.delete({ id: $routeParams.id }, function(video){
+                $location.path('/');
+            });
+        }
+    }]);
