0a5375c6d770b7cf5058cb24b3083927fddf9510
Pritam Sarkar <001pritam2012@gmail.com>
2016-05-10 00:06:00 +0530

    Created an API endpoint

--- vidzy-main/routes/videos.js
+++ vidzy-main/routes/videos.js
@@ -12,4 +12,16 @@ router.get('/', function(req, res) {
     });
 });
 
+router.post('/', function(req, res){
+    var collection = db.get('videos');
+    collection.insert({
+        title: req.body.title,
+        description: req.body.description
+    }, function(err, video){
+        if (err) throw err;
+
+        res.json(video);
+    });
+});
+
 module.exports = router;
