8cff09bfad4e0d259a1e88a4f130103a0c3f6eca
Pritam Sarkar <001pritam2012@gmail.com>
2016-05-09 20:23:27 +0530

    Implemented controller

--- vidzy-main/public/javascripts/vidzy.js
+++ vidzy-main/public/javascripts/vidzy.js
@@ -1,10 +1,19 @@
-var app = angular.module('Vidzy', ['ngRoute']);
+var app = angular.module('Vidzy', ['ngResource', 'ngRoute']);
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/', {
-            templateUrl: 'partials/home.html'
-        })
+        	templateUrl: 'partials/home.html',
+    		controller: 'HomeCtrl'
+		})
         .otherwise({
             redirectTo: '/'
         });
 }]);
+
+app.controller('HomeCtrl', ['$scope', '$resource', 
+    function($scope, $resource){
+        var Videos = $resource('/api/videos');
+        Videos.query(function(videos){
+            $scope.videos = videos;
+        });
+    }]);
